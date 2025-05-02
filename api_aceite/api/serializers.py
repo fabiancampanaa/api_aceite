306a4fc1b01b016a72ce27e1203b0e260d3c193f
Fabian Campana <f.campana@live.com>
2025-05-01 21:24:41 -0400

    gestion usuario ok

--- api_aceite/api/serializers.py
+++ api_aceite/api/serializers.py
@@ -10,7 +10,8 @@ class BusquedaSerializer(serializers.ModelSerializer):
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password', 'numero_telefono', 'tipo_usuario', 'tipo_acceso']
+        fields = ['id','username', 'email', 'password', 'numero_telefono', 'tipo_usuario', 'tipo_acceso']
+        
 
 class BusquedaRrssSerializer(serializers.ModelSerializer):
     class Meta:
